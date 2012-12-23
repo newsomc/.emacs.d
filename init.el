0911d512c20e146a4a75853e35dc70b1bf81a8c3
Robert Malko <robmalko@gmail.com>
2012-12-23 12:52:03 -0500

    add some load path directories

--- init.el
+++ init.el
@@ -5,3 +5,19 @@
 
 ;; turn off splash screen
 (setq inhibit-startup-message t)
+(setq make-backup-files nil)
+(setq auto-save-list-file-name nil)
+(setq auto-save-default nil)
+
+(setq user-emacs-directory "~/.emacs.d")
+(add-to-list 'load-path user-emacs-directory)
+
+(setq modules-dir (concat user-emacs-directory "modules"))
+(add-to-list 'load-path modules-dir)
+
+(setq themes-dir (concat user-emacs-directory "themes"))
+(add-to-list 'load-path themes-dir)
+
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
