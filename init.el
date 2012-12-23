124345aa57dc2a8bc8af5f09c5197120a439633f
Robert Malko <robmalko@gmail.com>
2012-12-23 12:25:43 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,7 @@
+;; turn off mouse interface
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; turn off splash screen
+(setq inhibit-startup-message t)
