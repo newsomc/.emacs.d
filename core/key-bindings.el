0378674d228891adea8a3848a90f60253836af99
Robert Malko <robmalko@gmail.com>
2012-12-24 10:58:49 -0500

    add a few buffer defuns

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -1,3 +1,6 @@
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
 (global-set-key "\C-xf" 'recentf-open-files)
 
 (provide 'key-bindings)
---
+++ defuns/defuns-buffer.el
@@ -0,0 +1,17 @@
+(defun create-scratch-buffer nil
+  "create a new scratch buffer to work in."
+  (interactive)
+  (let ((n 0)
+        bufname)
+    (while (progn
+             (setq bufname (concat "*scratch"
+                                   (if (= n 0) "" (int-to-string n))
+                                   "*"))
+             (setq n (1+ n))
+             (get-buffer bufname)))
+    (switch-to-buffer (get-buffer-create bufname))
+    (emacs-lisp-mode)))
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
