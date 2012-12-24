fbeb371acb66c7c68282fbf3e108c028eccfeb17
Robert Malko <robmalko@gmail.com>
2012-12-24 00:04:41 -0500

    adding more config options like hightlight current line, and adding background color to line numbers

--- core/appearance.el
+++ core/appearance.el
@@ -1,12 +1,25 @@
 (require 'linum)
-(global-linum-mode 1)
-(setq linum-format "%3d ")
-(set-face-attribute 'linum nil :background "#333")
 
-(setq line-number-mode t)
-(setq column-number-mode t)
-(setq fill-column 80)
+(auto-compression-mode t)
 
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-font-lock-mode t)
 (global-hl-line-mode 1)
+(global-linum-mode 1)
+
+(setq column-number-mode t)
+(setq delete-by-moving-to-trash t)
+(setq echo-keystrokes 0.1)
+(setq fill-column 80)
+(setq line-number-mode t)
+(setq linum-format "%3d ")
+(setq shift-select-mode nil)
+(setq x-select-enable-clipboard t)
+
+(set-face-attribute 'linum nil :background "#333")
+(set-face-background 'hl-line "#333")
+
+(show-paren-mode 1)
 
 (provide 'appearance)
