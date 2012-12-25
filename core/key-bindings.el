53dea540edede5765743026e88a5dde1aa76aae7
Robert Malko <robmalko@gmail.com>
2012-12-25 17:22:28 -0500

    missing help commands

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -15,11 +15,13 @@
 (global-set-key (kbd "C-c o") 'switch-to-next-window)
 
 (global-set-key (kbd "C-h h a") 'command-apropos)
+(global-set-key (kbd "C-h h c") 'describe-key-briefly)
 (global-set-key (kbd "C-h h f") 'describe-function)
 (global-set-key (kbd "C-h h k") 'describe-key)
 (global-set-key (kbd "C-h h m") 'describe-mode)
 (global-set-key (kbd "C-h h t") 'help-with-tutorial)
 (global-set-key (kbd "C-h h v") 'describe-variable)
+(global-set-key (kbd "C-h h w") 'where-is)
 
 (global-set-key (kbd "C-x C-b") 'list-existing-buffers)
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
