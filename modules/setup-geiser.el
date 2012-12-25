0129afb22e9da1713963de6e581cfbbc6da09cef
Robert Malko <robmalko@gmail.com>
2012-12-25 14:02:55 -0500

    geiser setup for racket

---
+++ modules/setup-geiser.el
@@ -0,0 +1,7 @@
+(setq geiser-active-implementations '(racket))
+(setq geiser-racket-binary
+  "/Applications/Racket v5.3.1/bin/racket")
+(setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
+(setq geiser-repl-startup-time 20000)
+
+(provide 'setup-geiser)
