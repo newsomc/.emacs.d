9ff705545a5fc215916dc773ae79cd6d65627016
Robert Malko <robmalko@gmail.com>
2012-12-25 16:57:54 -0500

    f2-f12 keys are now empty prefix keys; keybindings for bookmarks to C-h b

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -2,11 +2,19 @@
 (global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x m"))
 
+(-each '("<f2>" "<f3>" "<f4>" "<f5>" "<f6>" "<f7>"
+         "<f8>" "<f9>" "<f10>" "<f11>" "<f12>")
+       (lambda (key)
+         (global-unset-key (read-kbd-macro key))
+         (define-prefix-command (intern (concat key "-map")))
+         (global-set-key (read-kbd-macro key) (intern (concat key "-map")))))
+
 (define-prefix-command 'C-h-map)
 (global-set-key (kbd "C-h") 'C-h-map)
 
 (global-set-key (kbd "C-c o") 'switch-to-next-window)
 
+(global-set-key (kbd "C-h h a") 'command-apropos)
 (global-set-key (kbd "C-h h f") 'describe-function)
 (global-set-key (kbd "C-h h k") 'describe-key)
 (global-set-key (kbd "C-h h m") 'describe-mode)
--- modules/setup-bm.el
+++ modules/setup-bm.el
@@ -4,7 +4,7 @@
 
 (setq-default bm-buffer-persistence t)
 (setq-default bm-persistent-face
-  '(:foreground "#222222" :background "#b5bd68"))
+  '(:foreground "#333333" :background "#7099BC"))
 
 (add-hook' after-init-hook 'bm-repository-load)
 (add-hook 'find-file-hooks 'bm-buffer-restore)
@@ -18,6 +18,12 @@
 (add-hook 'after-revert-hook 'bm-buffer-restore)
 (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
 
-(global-set-key (kbd "<f2>") 'bm-toggle)
+(global-set-key (kbd "C-h b b") 'bm-toggle)
+(global-set-key (kbd "C-h b d") 'bm-remove-all-current-buffer)
+(global-set-key (kbd "C-h b l") 'bm-show-all)
+(global-set-key (kbd "C-h b n") 'bm-next)
+(global-set-key (kbd "C-h b p") 'bm-previous)
+(global-set-key (kbd "C-h b q") 'bm-remove-all-all-buffers)
+(global-set-key (kbd "C-h b t") 'bm-toggle)
 
 (provide 'setup-bm)
