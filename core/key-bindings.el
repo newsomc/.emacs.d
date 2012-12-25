7d33a58c5f236bbd84ac96733e372237ae7cc1cc
Robert Malko <robmalko@gmail.com>
2012-12-25 15:56:02 -0500

    add C-h as prefix key

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -2,6 +2,9 @@
 (global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x m"))
 
+(define-prefix-command 'C-h-map)
+(global-set-key (kbd "C-h") 'C-h-map)
+
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
 (global-set-key (kbd "C-x C-b") 'list-existing-buffers)
 (global-set-key (kbd "C-c o") 'switch-to-next-window)
