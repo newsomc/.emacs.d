6a7517d18bcea704fa5590481bb3dea56a9628eb
Robert Malko <robmalko@gmail.com>
2012-12-26 02:03:30 -0500

    buffer defuns for vi open line above/below and delete line

--- defuns/defuns-buffer.el
+++ defuns/defuns-buffer.el
@@ -21,6 +21,13 @@
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
+(defun kill-current-line (&optional n)
+  (interactive "p")
+  (save-excursion
+    (beginning-of-line)
+    (let ((kill-whole-line t))
+      (kill-line n))))
+
 (defun list-existing-buffers ()
   (interactive)
   (ibuffer-list-buffers)
@@ -30,3 +37,27 @@
 (defun untabify-buffer ()
   (interactive)
   (untabify (point-min) (point-max)))
+
+(defun vi-open-line-above ()
+  "Insert a newline above the current line and put point at beginning."
+  (interactive)
+  (unless (bolp)
+    (beginning-of-line))
+  (newline)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun vi-open-line-below ()
+  "Insert a newline below the current line and put point at beginning."
+  (interactive)
+  (unless (eolp)
+    (end-of-line))
+  (newline-and-indent))
+
+(defun vi-open-line (&optional abovep)
+  "Insert a newline below the current line and put point at beginning.
+With a prefix argument, insert a newline above the current line."
+  (interactive "P")
+  (if abovep
+      (vi-open-line-above)
+    (vi-open-line-below)))
