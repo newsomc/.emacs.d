db2efbf619b9d5647fb4a5290b968f779a57ee33
Robert Malko <robmalko@gmail.com>
2012-12-28 14:20:38 -0500

    smex keybinding

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -93,6 +93,7 @@
 ;;  M-. [jumps to the definition]
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x m") 'smex)
+(global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
