b2d23a239363c09f67e1dd441b145c323c98c761
Robert Malko <robmalko@gmail.com>
2013-01-04 08:41:37 -0500

    don't hl current line

--- core/appearance.el
+++ core/appearance.el
@@ -1,7 +1,7 @@
 (require 'linum)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-(global-hl-line-mode 1)
+; (global-hl-line-mode 1)
 
 (setq color-theme-is-global t
       column-number-mode t
@@ -21,7 +21,7 @@
 (setq-default truncate-lines t)
 
 (set-face-attribute 'linum nil :background "#222")
-(set-face-background 'hl-line "#333")
+; (set-face-background 'hl-line "#333")
 (set-face-foreground 'font-lock-warning-face "#ff6666")
 
 (show-paren-mode 1)
