6ee24401146cc2206c67f139005124a7d561f3d4
Robert Malko <robmalko@gmail.com>
2013-01-05 06:39:10 -0500

    adding some bk+ hooks to unset keys

--- core/mode-mappings.el
+++ core/mode-mappings.el
@@ -1,3 +1,11 @@
+;; bookmark+
+(add-hook 'bookmark-bmenu-mode-hook
+  (lambda ()
+    (local-unset-key (kbd "M-c"))
+    (local-unset-key (kbd "M-q"))
+    (local-unset-key (kbd "C-<up>"))
+    (local-unset-key (kbd "C-<down>"))))
+
 ;; lisp-mode
 (add-hook 'lisp-interaction-mode-hook
   (lambda ()
