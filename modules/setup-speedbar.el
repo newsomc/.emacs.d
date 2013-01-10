23c1edd92961bcc3e8d63a94b9eaff5c359aaa5b
Robert Malko <robmalko@gmail.com>
2013-01-10 17:40:58 -0500

    j/k bindings to move up/down in speedbar

--- modules/setup-speedbar.el
+++ modules/setup-speedbar.el
@@ -6,4 +6,7 @@
 (set-face-font 'speedbar-face "Inconsolata-12")
 (setq speedbar-mode-hook '(lambda () (buffer-face-set 'speedbar-face)))
 
+(define-key speedbar-mode-map (kbd "j") 'speedbar-next)
+(define-key speedbar-mode-map (kbd "k") 'speedbar-prev)
+
 (provide 'setup-speedbar)
