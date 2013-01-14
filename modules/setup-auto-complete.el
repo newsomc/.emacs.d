06b9b12447a6b8822f6ee7be34ee3894228a33a8
Robert Malko <robmalko@gmail.com>
2013-01-14 08:31:02 -0500

    auto-complete config

--- modules/setup-auto-complete.el
+++ modules/setup-auto-complete.el
@@ -1,6 +1,21 @@
 (require 'auto-complete)
+(require 'auto-complete-config)
 
 (global-auto-complete-mode t)
+(ac-config-default)
+
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(add-to-list 'ac-user-dictionary-files "~/.emacs.d/.dict")
+
+(setq ac-auto-show-menu 1.0)
+(setq ac-auto-start 3)
+(setq ac-ignore-case nil)
+(setq ac-menu-height 20)
+(setq ac-use-menu-map t)
+(setq ac-use-quick-help nil)
+
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
 
 (require 'semantic)
 (semantic-mode 1)
