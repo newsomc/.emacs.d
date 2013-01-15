0c83b6c6b3c03e94d3f93dedf3ec70e7dc1a4991
Robert Malko <robmalko@gmail.com>
2013-01-15 18:05:54 -0500

    more speedbar config; smls snippets

--- modules/setup-speedbar.el
+++ modules/setup-speedbar.el
@@ -24,8 +24,10 @@
              (define-key speedbar-mode-map [right] 'speedbar-flush-expand-line)
              (define-key speedbar-mode-map [left] 'speedbar-contract-line)))
 
+(define-key speedbar-mode-map (kbd "h") 'speedbar-up-directory)
 (define-key speedbar-mode-map (kbd "j") 'speedbar-next)
 (define-key speedbar-mode-map (kbd "k") 'speedbar-prev)
+(define-key speedbar-mode-map (kbd "l") 'speedbar-flush-expand-line)
 (define-key speedbar-mode-map (kbd "o") 'speedbar-edit-line)
 (define-key speedbar-mode-map (kbd "C-j C-l") 'ace-jump-mode)
 (define-key speedbar-mode-map (kbd "C-j ll") 'ace-jump-mode)
