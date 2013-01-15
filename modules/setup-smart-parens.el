66915cb718270d90202548ff4423e52dfb770186
Robert Malko <robmalko@gmail.com>
2013-01-15 18:29:35 -0500

    more smart-parens config; turned off coloring

--- modules/setup-smart-parens.el
+++ modules/setup-smart-parens.el
@@ -1,4 +1,11 @@
 (smartparens-global-mode 1)
+(show-smartparens-global-mode t)
+
+(setq sp-highlight-pair-overlay nil)
+(setq sp-highlight-wrap-overlay nil)
+(setq sp-highlight-wrap-tag-overlay nil)
 (setq sp-navigate-consider-symbols t)
+
+(sp-add-pair "(*" "*)")
 
 (provide 'setup-smart-parens)
