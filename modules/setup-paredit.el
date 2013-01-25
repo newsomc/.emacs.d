a45111aabff7a9be08edd7db85fb0d2602695d24
Robert Malko <robmalko@gmail.com>
2013-01-25 11:41:46 -0500

    paredit mode is in

---
+++ modules/setup-paredit.el
@@ -0,0 +1,43 @@
+(require 'paredit)
+
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+(add-hook 'geiser-repl-mode-hook      (lambda () (paredit-mode +1)))
+
+(defun paredit--is-at-start-of-sexp ()
+  (and (looking-at "(\\|\\[")
+       (not (nth 3 (syntax-ppss))) ;; inside string
+       (not (nth 4 (syntax-ppss))))) ;; inside comment
+
+(defun paredit-duplicate-closest-sexp ()
+  (interactive)
+  ;; skips to start of current sexp
+  (while (not (paredit--is-at-start-of-sexp))
+    (paredit-backward))
+  (set-mark-command nil)
+  ;; while we find sexps we move forward on the line
+  (while (and (bounds-of-thing-at-point 'sexp)
+              (<= (point) (car (bounds-of-thing-at-point 'sexp)))
+              (not (= (point) (line-end-position))))
+    (forward-sexp)
+    (while (looking-at " ")
+      (forward-char)))
+  (kill-ring-save (mark) (point))
+  ;; go to the next line and copy the sexp's we encountered
+  (paredit-newline)
+  (yank)
+  (exchange-point-and-mark))
+
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
+(define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind)
+(define-key paredit-mode-map (kbd "M-q") 'save-buffers-kill-terminal)
+
+(provide 'setup-paredit)
