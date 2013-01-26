b5be6a97db8d58a5a8ca88c3b8e0355e68c6ccb9
Robert Malko <robmalko@gmail.com>
2013-01-26 08:59:13 -0500

    eval-defun / region kb

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -137,6 +137,8 @@
 
 ;; eval-elisp
 (global-set-key (kbd "C-j eb") 'eval-buffer)
+(global-set-key (kbd "C-j ee") 'eval-defun)
+(global-set-key (kbd "C-j er") 'eval-region)
 
 ;; expand-region
 (global-set-key (kbd "C-;") 'er/expand-region)
