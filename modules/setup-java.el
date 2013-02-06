76a9981624c57a7fd419b5187ad3f5c292d7773d
Robert Malko <robmalko@gmail.com>
2013-02-06 06:41:12 -0500

    emacs-eclip for when I need to do java

--- modules/setup-java.el
+++ modules/setup-java.el
@@ -1,3 +1,19 @@
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-eclim/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-eclim/vendor"))
+
+(require 'eclim)
+(require 'eclimd)
+
+(setq eclim-auto-save t)
+(global-eclim-mode)
+
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
+
 (add-hook 'java-mode-hook (lambda ()
   (define-key java-mode-map (kbd "M-q") 'save-buffers-kill-terminal)))
 
